825d12e1321c01b6dd1b0eed02089e579e83f063
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-13 14:12:53 +0430

    centralize the import/export of images

---
+++ src/assets/icons/indes.js
@@ -0,0 +1,6 @@
+import check from "./icon-check.svg";
+import cross from "./icon-cross.svg";
+import moon from "./icon-moon.svg";
+import sun from "./icon-sun.svg";
+
+export default { check, cross, moon, sun };
---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+import desktopDark from "./bg-desktop-dark.jpg";
+import desktopLight from "./bg-desktop-light.jpg";
+import mobileDark from "./bg-mobile-dark.jpg";
+import mobileLight from "./bg-mobile-light.jpg";
+
+export default { desktopDark, desktopLight, mobileDark, mobileLight };
