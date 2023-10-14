c3297c0784fbb145d9473e49439996810af55fa7
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-14 16:41:20 +0430

    Move image to the public folder in order to  access them in the styles files

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,4 +3,4 @@ import cross from "./icon-cross.svg";
 import moon from "./icon-moon.svg";
 import sun from "./icon-sun.svg";
 
-export default { check, cross, moon, sun };
+export { check, cross, moon, sun };
