ff2f9e0acd48a3ace834540c3942de28257cc45e
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-18 18:46:10 +0430

    Change app theme according to user's device theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/header/Header";
 import Todo from "./features/todo/TodoWrapper";
 import { ThemeProvider } from "styled-components";
@@ -9,6 +9,11 @@ import Main from "./components/main/Main";
 
 const App = () => {
   const [currentTheme, setCurrentTheme] = useState("light");
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+      setCurrentTheme("dark");
+    }
+  }, []);
   return (
     <ThemeProvider
       theme={currentTheme === "light" ? theme.lightTheme : theme.darkTheme}
